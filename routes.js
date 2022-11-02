30ab41b862f793ea846a1ace6697270775d2579d
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-02 12:43:58 -0300

    feat: last tweets object

--- routes.js
+++ routes.js
@@ -1,9 +1,25 @@
 import express from 'express';
+import fs from 'fs';
 
 const routes = express.Router();
 
 routes.get('/tweets', (req, res) => {
-  res.send('OK');
+  const pathData = 'database.json';
+  const encoding = 'utf-8';
+
+  fs.readFile(pathData, encoding, (err, data) => {
+    if (err) throw err;
+
+    const allTweets = JSON.parse(data);
+    const smallDatabase = allTweets.length <= 10;
+    let latestTweets;
+
+    (smallDatabase)
+      ? latestTweets = allTweets
+      : latestTweets = allTweets.slice(allTweets.length - 10);
+
+    res.send(latestTweets);
+  });
 });
 
 export default routes;
