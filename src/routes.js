e7ac9a8f832f2faa7aed8edba8db1922488d6ebd
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-03 15:46:35 -0300

    refactor: separate tweet to user controllers

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import express from 'express';
-import { index, sendMessage, makeLogin } from './controllers/TweetControllers.js';
+import { index, sendMessage } from './controllers/TweetControllers.js';
+import { makeLogin, showUser } from './controllers/UserControllers.js';
 
 const routes = express.Router();
 
 routes.get('/tweets', index);
 
 routes.post('/tweets', sendMessage);
+
+routes.post('/tweets/:user', showUser);
 
 routes.post('/sign-up', makeLogin);
 
