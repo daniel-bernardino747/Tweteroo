8c0da8a746bf0fc5bc83f0472bb860adfe00e541
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-03 10:08:56 -0300

    feat(user/controller): add post method to /sign-up

--- src/controllers/TweetControllers.js
+++ src/controllers/TweetControllers.js
@@ -43,10 +43,51 @@ function sendMessage(request, response) {
       });
     });
 
-    return response.send('OK');
+    return response.status(201).send('OK');
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 }
 
-export { index, sendMessage };
+function makeLogin(request, response) {
+  const { username, avatar } = request.body;
+
+  if (!avatar || !username) {
+    return response.status(400).json({ error: 'Missing username or tweet.' });
+  }
+
+  try {
+    const url = new URL(avatar);
+  } catch (err) {
+    return response.status(404).json({ error: 'Invalid URL' });
+  }
+
+  try {
+    const pathTweets = './src/datas/usersData.json';
+    const encoding = 'utf-8';
+
+    fs.readFile(pathTweets, encoding, (err, data) => {
+      if (err) throw err;
+
+      const allUsers = JSON.parse(data);
+
+      const existingUser = allUsers.find((account) => account.username === username);
+
+      console.log(existingUser);
+
+      if (!existingUser) {
+        allUsers.push(request.body);
+        const updateUsers = JSON.stringify(allUsers);
+        fs.writeFile(pathTweets, updateUsers, (error) => {
+          if (error) throw error;
+        });
+        return response.status(201).send('OK');
+      }
+      return response.status(409).json({ error: 'This user already exists.' });
+    });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+}
+
+export { index, sendMessage, makeLogin };
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { index, sendMessage } from './controllers/TweetControllers.js';
+import { index, sendMessage, makeLogin } from './controllers/TweetControllers.js';
 
 const routes = express.Router();
 
 routes.get('/tweets', index);
 
 routes.post('/tweets', sendMessage);
+
+routes.post('/sign-up', makeLogin);
 
 export default routes;
